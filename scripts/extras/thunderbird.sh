767fbba5904b378cbdb6cdc04fc68af63a6b9880
Raul Dipeas <github@rauldipeas.com.br>
2025-08-10 05:55:19 -0300

    ^thunderbird

--- scripts/extras/thunderbird.sh
+++ scripts/extras/thunderbird.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 set -e
 # shellcheck disable=SC2034
-DEPS='thunderbird-locale-pt-br'
+BASENAME='thunderbird'
+# shellcheck disable=SC2034
+LN='thunderbird'
+# shellcheck disable=SC2034
+EXEC_OLD='thunderbird'
+# shellcheck disable=SC2034
+EXEC_NEW='env GDK_BACKEND=x11 thunderbird'
+# shellcheck disable=SC2034
+DEPS='systray-x-gnome thunderbird-locale-pt-br'
 # shellcheck disable=SC2034
 PPA='mozillateam/ppa'
 # shellcheck disable=SC2034
 INSTNAME='thunderbird-gnome-support'
 # shellcheck disable=SC1090
 source <(curl -s https://rauldipeas.com.br/uds/functions.sh)
+wget -qO- https://download.opensuse.org/repositories/home:/Ximi1970/xUbuntu_24.04/Release.key | sudo tee /etc/apt/trusted.gpg.d/Systray-x.Ximi1970.asc >/dev/null
+printf 'deb https://download.opensuse.org/repositories/home:/Ximi1970:/Mozilla:/Add-ons/xUbuntu_24.04 ./' | sudo tee /etc/apt/sources.list.d/systray-x.list >/dev/null
+sudo apt update
 add_ppa
-cat <<EOF | sudo tee /etc/apt/preferences.d/thunderbird
+cat <<EOF | sudo tee /etc/apt/preferences.d/thunderbird >/dev/null
 Package: thunderbird*
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
 EOF
+fix_launcher
 install_deb
