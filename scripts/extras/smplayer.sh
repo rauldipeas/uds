16c28021452a99d56c434198f20bdeed68de2045
Raul Dipeas <github@rauldipeas.com.br>
2025-08-13 13:38:24 -0300

    +smplayer -celluloid

--- scripts/extras/smplayer.sh
+++ scripts/extras/smplayer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 # shellcheck disable=SC2034
-INSTNAME='celluloid'
+INSTNAME='smplayer'
 # shellcheck disable=SC1090
 source <(curl -s https://rauldipeas.com.br/uds/functions.sh)
 install_deb
 grep -E '^(audio|video)/' /usr/share/mime/types | cut -d: -f1 |
     while IFS= read -r type; do
-        xdg-mime default io.github.celluloid_player.Celluloid.desktop "$type"
+        xdg-mime default smplayer.desktop "$type"
     done
