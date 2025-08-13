507c4be9a899e969f4241f45b79c55c5486a04e5
Raul Dipeas <github@rauldipeas.com.br>
2025-08-13 06:26:58 -0300

    ^superpaper

--- scripts/extras/superpaper.sh
+++ scripts/extras/superpaper.sh
@@ -6,8 +6,8 @@ DEPS='python3-wxgtk4.0'
 source <(curl -s https://rauldipeas.com.br/uds/functions.sh)
 install_deb
 pipx install --force superpaper --system-site-packages
-mkdir -p "$HOME"/.local/share/icons
-cp "$HOME"/.local/share/pipx/venvs/superpaper/share/icons/hicolor/256x256/apps/superpaper.png "$HOME"/.local/share/icons/
+mkdir -p "$HOME"/.local/share/{applications,icons}
+ln -fs "$HOME"/.local/share/pipx/venvs/superpaper/share/icons/hicolor/256x256/apps/superpaper.png "$HOME"/.local/share/icons/
 cat <<EOF | tee "$HOME"/.local/share/applications/superpaper.desktop >/dev/null
 [Desktop Entry]
 Type=Application
