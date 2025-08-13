44415f520de172fbf133e5d28caac031db2c3bd9
Raul Dipeas <github@rauldipeas.com.br>
2025-08-13 12:05:25 -0300

    ^proton-vpc ^syncthing

--- scripts/extras/syncthing.sh
+++ scripts/extras/syncthing.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 # shellcheck disable=SC2034
-INSTNAME='syncthing'
+TARGET="$(curl -s https://api.github.com/repos/syncthing/syncthing/releases | grep browser_download_url | grep amd64.deb | head -n1 | cut -d '"' -f4)"
 # shellcheck disable=SC1090
 source <(curl -s https://rauldipeas.com.br/uds/functions.sh)
+enter_tmp
+download
 install_deb
