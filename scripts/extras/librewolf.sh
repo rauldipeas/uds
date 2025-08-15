4a522d89e417737ec776a6d8a49f0fc8156a0467
Raul Dipeas <github@rauldipeas.com.br>
2025-08-15 12:38:02 -0300

    ^librewolf

--- scripts/extras/librewolf.sh
+++ scripts/extras/librewolf.sh
@@ -4,3 +4,5 @@ sudo apt install -y --reinstall extrepo
 sudo extrepo enable librewolf
 sudo apt update
 sudo apt install -y --reinstall chrome-gnome-shell librewolf
+xdg-mime default librewolf.desktop x-scheme-handler/http
+xdg-mime default librewolf.desktop x-scheme-handler/https
