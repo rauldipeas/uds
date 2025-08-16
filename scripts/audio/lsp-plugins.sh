1617c37b47eeb33c83cc0433b964c86d5232cee9
Raul Dipeas <github@rauldipeas.com.br>
2025-08-16 07:49:38 -0300

    ^lsp-plugins

--- scripts/audio/lsp-plugins.sh
+++ scripts/audio/lsp-plugins.sh
@@ -2,7 +2,7 @@
 set -e
 cd /tmp
 rm -f /tmp/*.7z
-wget -q --show-progress "$(curl -sSL https://api.github.com/repos/lsp-plugins/lsp-plugins/releases | grep browser_download_url | grep download | grep Linux-x86_64 | head -n1 | cut -d '"' -f4)"
+wget -q --show-progress "$(curl -sL https://api.github.com/repos/lsp-plugins/lsp-plugins/releases | grep browser_download_url | grep download | grep Linux-x86_64 | head -n1 | cut -d '"' -f4)"
 7za x -y -bsp0 lsp-plugins-*-Linux-x86_64.7z >/dev/null
 mkdir -p "$HOME"/.vst3
 rm -fr "$HOME"/.vst3/lsp-plugins.vst3
