3e10fd3209dec9a878f92f86a1a205239627fc99
Raul Dipeas <github@rauldipeas.com.br>
2025-08-16 13:17:39 -0300

    ^obs-studio

--- scripts/video/obs-studio.sh
+++ scripts/video/obs-studio.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
 printf 2|am -i --icons obs-studio
+if [ "$(gsettings get org.gnome.desktop.interface icon-theme)" == "'Papirus-Dark'" ]; then
+    mkdir -p "$HOME"/.icons/Papirus-Dark/64x64/apps
+    ln -fs /usr/share/icons/Papirus-Dark/64x64/apps/obs.svg "$HOME"/.icons/Papirus-Dark/64x64/apps/obs-studio.svg
+fi
